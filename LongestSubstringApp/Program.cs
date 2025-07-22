c8a42a0a32df6e302b5889dda67a9a2ea11deb5d
Rangi <2309580@cognizant.com>
2025-07-22 13:08:33 +0530

    Longest Substring without repeating Character

---
+++ LongestSubstringApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program{
+    public static void Main(string[] args){
+        string str = "abcbcacab";
+        HashSet<char> st = new HashSet<char>();
+
+        int maxLength = 0;
+        int left = 0;
+        for(int right = 0; right < str.Length; right++){
+            while(st.Contains(str[right])){
+                st.Remove(str[left]);
+                left++;
+            }
+
+            st.Add(str[right]);
+            maxLength = Math.Max(maxLength, right -left +1);
+        }
+    Console.WriteLine(maxLength);
+    }
+}
